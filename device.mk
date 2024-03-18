fb5654565d640122a1190f9974519ebaaf734331
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2024-03-18 14:42:13 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -76,9 +76,6 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
-TW_OVERRIDE_SYSTEM_PROPS := \
-    "ro.build.product;ro.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
-
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
